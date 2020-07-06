dadb5e10a668d6b404c6bc5aa212bf6fb10966a3
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2020-07-06 21:23:46 +0200

    Handle SemVer pre-release dot separated variants

--- build.gradle.kts
+++ build.gradle.kts
@@ -107,6 +107,6 @@ tasks {
     publishPlugin {
         dependsOn("patchChangelog")
         token(System.getenv("PUBLISH_TOKEN"))
-        channels(pluginVersion.split('-').getOrElse(1) { "default" })
+        channels(pluginVersion.split('-').getOrElse(1) { "default" }.split('.').first())
     }
 }
