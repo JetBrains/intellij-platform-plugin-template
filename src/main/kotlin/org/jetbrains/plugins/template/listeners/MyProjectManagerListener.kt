e3e031bd96343ffafe6d0a7b2bc712f0829d8d37
Casper Boone <mail@casperboone.nl>
2020-10-30 17:05:21 +0100

    Use Kotlin extension function to retrieve service

--- src/main/kotlin/org/jetbrains/plugins/template/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/listeners/MyProjectManagerListener.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.template.listeners
 
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
 import org.jetbrains.plugins.template.services.MyProjectService
@@ -7,6 +8,6 @@ import org.jetbrains.plugins.template.services.MyProjectService
 internal class MyProjectManagerListener : ProjectManagerListener {
 
     override fun projectOpened(project: Project) {
-        project.getService(MyProjectService::class.java)
+        project.service<MyProjectService>()
     }
 }
