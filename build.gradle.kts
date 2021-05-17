e85e446356ad75dbea63d768f28e54b969191c56
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-05-17 14:04:30 +0200

    Remove `jcenter()` from repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,6 @@ version = properties("pluginVersion")
 // Configure project's dependencies
 repositories {
     mavenCentral()
-    jcenter()
 }
 dependencies {
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.17.0")
