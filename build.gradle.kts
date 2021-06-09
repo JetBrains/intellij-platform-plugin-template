006716ca0f7bdd8e50a284ba1f7de7f32d206846
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-06-09 23:09:03 +0200

    ktlint fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.changelog.markdownToHTML
-import org.jetbrains.intellij.tasks.DownloadRobotServerPluginTask
-import org.jetbrains.intellij.tasks.RunIdeForUiTestTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun properties(key: String) = project.findProperty(key).toString()
