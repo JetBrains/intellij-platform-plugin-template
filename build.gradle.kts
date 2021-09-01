59164496f2c815c3d70e3618d4e5a0b11a9eac04
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-09-01 23:26:54 +0200

    Qodana integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.0"
     // Gradle Qodana Plugin
-    id("org.jetbrains.qodana") version "0.1.11"
+    id("org.jetbrains.qodana") version "0.1.12"
 }
 
 group = properties("pluginGroup")
