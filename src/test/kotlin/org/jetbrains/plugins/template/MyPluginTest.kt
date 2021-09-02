8e121f90bf1eb7c69810e60e7bb7259283b529f8
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-09-02 12:28:24 +0200

    tests

---
+++ src/test/kotlin/org/jetbrains/plugins/template/MyPluginTest.kt
@@ -0,0 +1,30 @@
+package org.jetbrains.plugins.template
+
+import com.intellij.ide.highlighter.XmlFileType
+import com.intellij.psi.xml.XmlFile
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.intellij.util.PsiErrorElementUtil
+
+class MyPluginTest : BasePlatformTestCase() {
+
+    fun `test created XML file`() {
+        val psiFile = myFixture.configureByText(XmlFileType.INSTANCE, "<foo>bar</foo>")
+        assertInstanceOf(psiFile, XmlFile::class.java)
+
+        assertFalse(PsiErrorElementUtil.hasErrors(project, psiFile.virtualFile))
+
+        psiFile as XmlFile
+        assertNotNull(psiFile.rootTag)
+
+        psiFile.rootTag?.let {
+            assertEquals("foo", it.name)
+            assertEquals("bar", it.value.text)
+        }
+    }
+
+    override fun getTestDataPath() = "src/test/resources/rename"
+
+    fun `test renaming XML tag`() {
+        myFixture.testRename("foo.xml", "foo_after.xml", "a2")
+    }
+}
