be7db8265fd77f0f8f7ffd887541ab7a058e61f5
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-09-02 15:05:13 +0200

    Qodana integration

--- src/main/kotlin/org/jetbrains/plugins/template/MyBundle.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/MyBundle.kt
@@ -1,6 +1,5 @@
 package org.jetbrains.plugins.template
 
-import com.intellij.AbstractBundle
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
@@ -15,7 +14,7 @@ object MyBundle : DynamicBundle(BUNDLE) {
     fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
         getMessage(key, *params)
 
-    @Suppress("SpreadOperator")
+    @Suppress("SpreadOperator", "unused")
     @JvmStatic
     fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
         getLazyMessage(key, *params)
