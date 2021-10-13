a9b6bfc11dc15fe1b1f9b4d09ff7e2bfd43a152e
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-10-13 23:19:30 +0200

    Dependencies - upgrade `org.jetbrains.changelog` to `1.3.1`

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     // Gradle IntelliJ Plugin
     id("org.jetbrains.intellij") version "1.2.0"
     // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "1.3.0"
+    id("org.jetbrains.changelog") version "1.3.1"
     // Gradle Qodana Plugin
     id("org.jetbrains.qodana") version "0.1.13"
 }
