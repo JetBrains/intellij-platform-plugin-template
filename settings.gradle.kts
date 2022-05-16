1f2246cdf814cd0e3044487c6af23be78c92d5df
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-05-16 12:31:17 +0200

    1.6.0-SNAPSHOT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "IntelliJ Platform Plugin Template"
+
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+        }
+        gradlePluginPortal()
+    }
+}
