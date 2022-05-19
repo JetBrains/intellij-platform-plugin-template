0644b59d8eb62fa6891c90f04cc0bfc83d73518a
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-05-19 09:25:53 +0200

    Example implementation: invoke `TODO()` only if not CI

--- src/main/kotlin/org/jetbrains/plugins/template/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/listeners/MyProjectManagerListener.kt
@@ -10,6 +10,7 @@ internal class MyProjectManagerListener : ProjectManagerListener {
     override fun projectOpened(project: Project) {
         project.service<MyProjectService>()
 
-        TODO("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+        System.getenv("CI")
+            ?: TODO("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
     }
 }
