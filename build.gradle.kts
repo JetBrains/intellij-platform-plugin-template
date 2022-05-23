6668091e9212e4b8044db5db126bc8921a08afe8
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-05-23 12:16:04 +0200

    Dependencies - upgrade `org.jetbrains.intellij` to `1.6.0`

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.6.21"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.5.3"
+    id("org.jetbrains.intellij") version "1.6.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.1"
     // Gradle Qodana Plugin
