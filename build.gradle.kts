c9937581e6d1b0c42218814cd7b7953edbea48a4
Leonard Brünings <leonard.bruenings@gradle.com>
2023-01-27 14:28:15 +0100

    Fix configuration cache issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,6 +85,7 @@ tasks {
             }
         )
 
+        val changelog = project.changelog // local variable for configuration cache compatibility
         // Get the latest available change notes from the changelog file
         changeNotes.set(properties("pluginVersion").map { pluginVersion ->
             with(changelog) {
