c00c39163a6db9b0e14657491926861c330372b9
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2023-03-03 17:13:51 +0100

    Use logger warn instead of `TODO()`

--- src/main/kotlin/org/jetbrains/plugins/template/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/listeners/MyFrameStateListener.kt
@@ -6,9 +6,6 @@ import com.intellij.openapi.diagnostic.thisLogger
 internal class MyFrameStateListener : FrameStateListener {
 
     override fun onFrameActivated() {
-        thisLogger().info("Frame activated")
-
-        System.getenv("CI")
-            ?: TODO("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
     }
 }
