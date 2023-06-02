bc4bf88f992a94ec1a903afc120b86d2f9cb0dd8
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2023-06-02 13:04:19 +0200

    Use Java `17` for JVM Toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(17)
 }
 
 // Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
