db8f493cc18cc9ee56e31e124435ef00f86bc0a1
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2024-03-08 13:42:46 +0100

    Gradle - Removed Qodana Gradle Plugin configuration to rely on defaults

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,14 +47,6 @@ changelog {
     repositoryUrl = properties("pluginRepositoryUrl")
 }
 
-// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
-qodana {
-    cachePath = provider { file(".qodana").canonicalPath }
-    reportPath = provider { file("build/reports/inspections").canonicalPath }
-    saveReport = true
-    showReport = environment("QODANA_SHOW_REPORT").map { it.toBoolean() }.getOrElse(false)
-}
-
 // Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
 koverReport {
     defaults {
