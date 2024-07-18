fefb39725a2a9cabe52877d81b9372d1b5881796
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2024-07-18 16:56:12 +0200

    Add `junit:junit:4.13.2` to test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ repositories {
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
-    // implementation(libs.exampleLibrary)
+    testImplementation(libs.junit)
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
