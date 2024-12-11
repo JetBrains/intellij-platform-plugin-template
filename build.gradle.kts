60e566d609a150353f368e572b991e612959fac3
Jakub Chrzanowski <jakub@chrzanowski.info>
2024-12-11 19:01:25 +0100

    Remove redundant IntelliJ Platform dependency helpers: `instrumentationTools()`, `pluginVerifier()`, `zipSigner()`

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,9 +43,6 @@ dependencies {
         // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file for plugin from JetBrains Marketplace.
         plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
 
-        instrumentationTools()
-        pluginVerifier()
-        zipSigner()
         testFramework(TestFrameworkType.Platform)
     }
 }
