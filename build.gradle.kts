48e88ac4aff2d93fc34791fd3608fffe395fb758
Jakub Chrzanowski <jakub@chrzanowski.info>
2025-03-28 20:25:28 +0100

    Added `opentest4j` test dependency, see: Missing opentest4j dependency in Test Framework (https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-faq.html#missing-opentest4j-dependency-in-test-framework)

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ repositories {
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
     testImplementation(libs.junit)
+    testImplementation(libs.opentest4j)
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
