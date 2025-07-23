9a6945d9e19e7bcfd5d2e3b8f8a83fc387195d97
Nebojsa Vuksic <nebojsa.vuksic@jetbrains.com>
2025-07-23 14:52:52 +0200

    Delete MyProjectService.kt

--- src/main/kotlin/org/jetbrains/plugins/template/services/MyProjectService.kt
+++
@@ -1,8 +0,0 @@
-package org.jetbrains.plugins.template.services
-
-import com.intellij.openapi.components.Service
-
-@Service
-class MyProjectService() {
-    fun getRandomNumber() = (1..100).random()
-}
