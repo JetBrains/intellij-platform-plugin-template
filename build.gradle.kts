13a0fb863b3d802906e5aedcde588b436884f42b
vldf <vladislavf7@gmail.com>
2025-08-07 11:21:56 +0300

    Add `platformBundledModules` to gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
         // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file for plugin from JetBrains Marketplace.
         plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
 
+        bundledModules(providers.gradleProperty("platformBundledModules").map { it.split(',') })
+
         testFramework(TestFrameworkType.Platform)
     }
 }
