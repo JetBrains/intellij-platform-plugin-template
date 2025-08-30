3fe1fff22562379666851719733a1ab86f2d265d
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2025-08-30 10:08:07 +0200

    Update icon and template name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "IntelliJ Plugin with Compose"
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
